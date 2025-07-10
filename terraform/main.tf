98ebdf11ea45b680b8330c21d60203bcf980442e
lmunarim <lmunarim@gmail.com>
2025-07-10 17:15:57 -0300

    teste

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,22 @@
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_dir  = "/home/runner/work/_temp/publish/"
+  output_path = "lambda.zip"
+  depends_on  = [null_resource.build_dotnet_lambda]
+}
+
+resource "aws_lambda_function" "dotnet8_consumer" {
+  depends_on    = [data.archive_file.lambda]
+  function_name = "lambda-status"
+  filename      = "lambda.zip" # Path to your zipped .NET 8 Lambda
+  handler       = "LambdaStatus::LambdaStatus.Function::FunctionHandler"
+  runtime       = "dotnet8"
+  role          = aws_iam_role.lambda_exec_role.arn
+  source_code_hash = data.archive_file.lambda.output_base64sha256 # ?
+  #source_code_hash = filebase64sha256("publish/lambda.zip")
+}
+
+
 resource "aws_sqs_queue" "sqs-notifications" {
   name                      = "tech-challenge-fiap-upload-notifications"
   visibility_timeout_seconds = 30
@@ -47,24 +66,6 @@ resource "aws_iam_policy" "lambda_sqs_policy" {
 resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = aws_iam_policy.lambda_sqs_policy.arn
-}
-
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_dir  = "/home/runner/work/_temp/publish/"
-  output_path = "lambda.zip"
-  depends_on  = [null_resource.build_dotnet_lambda]
-}
-
-resource "aws_lambda_function" "dotnet8_consumer" {
-  depends_on    = [data.archive_file.lambda]
-  function_name = "lambda-status"
-  filename      = "lambda.zip" # Path to your zipped .NET 8 Lambda
-  handler       = "LambdaStatus::LambdaStatus.Function::FunctionHandler"
-  runtime       = "dotnet8"
-  role          = aws_iam_role.lambda_exec_role.arn
-  source_code_hash = data.archive_file.lambda.output_base64sha256 # ?
-  #source_code_hash = filebase64sha256("publish/lambda.zip")
 }
 
 resource "aws_lambda_event_source_mapping" "sqs_trigger" {
