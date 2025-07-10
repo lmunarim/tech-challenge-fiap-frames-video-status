b9a14905161b7ebf791435862c85ad2b86412c8d
lmunarim <lmunarim@gmail.com>
2025-07-10 16:33:24 -0300

    teste

--- terraform/build.tf
+++ terraform/build.tf
@@ -2,8 +2,8 @@ resource "null_resource" "build_dotnet_lambda" {
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command = <<-EOT
-      dotnet restore "${path.module}/src/Lambda/LambdaStatus/LambdaStatus.csproj"
-      dotnet publish "${path.module}/src/Lambda/LambdaStatus/LambdaStatus.csproj" -c Release -o "/home/runner/work/_temp/publish"
+      dotnet restore "./src/Lambda/LambdaStatus/LambdaStatus.csproj"
+      dotnet publish "./src/Lambda/LambdaStatus/LambdaStatus.csproj" -c Release -o "/home/runner/work/_temp/publish"
     EOT
   }
 }
