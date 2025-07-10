3ff36cfc7409bbe3c23a9cd785be1b03533ef05e
lmunarim <lmunarim@gmail.com>
2025-07-10 19:10:37 -0300

    teste

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,7 +69,8 @@ resource "aws_iam_policy" "lambda_sqs_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
-  role       = aws_iam_role.lambda_exec_role.name
+  #role       = aws_iam_role.lambda_exec_role.name
+  role       = data.aws_iam_role.lambda_exec_role.name
   policy_arn = aws_iam_policy.lambda_sqs_policy.arn
 }
 
