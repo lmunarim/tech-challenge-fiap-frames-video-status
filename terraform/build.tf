b866d1387fe3c454ba03898eb59e4e861220ec90
lmunarim <lmunarim@gmail.com>
2025-07-10 16:14:44 -0300

    teste

--- terraform/build.tf
+++ terraform/build.tf
@@ -3,8 +3,8 @@ resource "null_resource" "build_dotnet_lambda" {
     interpreter = ["/bin/sh", "-c"]
 
     command = <<-EOT
-      dotnet restore "${path.module}/src/Lambda/LambdaStatus/LambdaStatus.csproj" -t:rebuild
-      dotnet publish "${path.module}/src/Lambda/LambdaStatus/LambdaStatus.csproj" -t:rebuild -c Release -o "/home/runner/work/_temp/publish"
+      dotnet restore "./src/Lambda/LambdaStatus/LambdaStatus.csproj" -t:rebuild
+      dotnet publish "./src/Lambda/LambdaStatus/LambdaStatus.csproj" -t:rebuild -c Release -o "/home/runner/work/_temp/publish"
     EOT
   }
 }
