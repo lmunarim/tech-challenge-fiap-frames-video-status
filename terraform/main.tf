724271c564274199897f7238f0dcb3323ae131d2
lmunarim <lmunarim@gmail.com>
2025-07-11 00:25:27 -0300

    teste

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,54 +24,54 @@ resource "aws_lambda_function" "dotnet8_consumer" {
 #   message_retention_seconds = 86400
 # }
 
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda-status-not-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
+# resource "aws_iam_role" "lambda_exec_role" {
+#   name = "lambda-status-not-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [{
+#       Effect = "Allow",
+#       Principal = {
+#         Service = "lambda.amazonaws.com"
+#       },
+#       Action = "sts:AssumeRole"
+#     }]
+#   })
+# }
 
-resource "aws_iam_policy" "lambda_sqs_policy" {
-  name = "lambda-status-not-sqs-policy"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "sqs:ReceiveMessage",
-          "sqs:DeleteMessage",
-          "sqs:GetQueueAttributes"
-        ],
-        Resource = "*"
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
+# resource "aws_iam_policy" "lambda_sqs_policy" {
+#   name = "lambda-status-not-sqs-policy"
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Action = [
+#           "sqs:ReceiveMessage",
+#           "sqs:DeleteMessage",
+#           "sqs:GetQueueAttributes"
+#         ],
+#         Resource = "*"
+#       },
+#       {
+#         Effect = "Allow",
+#         Action = [
+#           "logs:CreateLogGroup",
+#           "logs:CreateLogStream",
+#           "logs:PutLogEvents"
+#         ],
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
-  role = aws_iam_role.lambda_exec_role.name
-  policy_arn = aws_iam_policy.lambda_sqs_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+#   role = aws_iam_role.lambda_exec_role.name
+#   policy_arn = aws_iam_policy.lambda_sqs_policy.arn
+# }
 
-resource "aws_lambda_event_source_mapping" "sqs_trigger" {
-  event_source_arn = "arn:aws:sqs:us-east-1:147997141255:tech-challenge-fiap-upload-notifications"
-  function_name    = aws_lambda_function.dotnet8_consumer.arn
-  batch_size       = 10
-}
+# resource "aws_lambda_event_source_mapping" "sqs_trigger" {
+#   event_source_arn = "arn:aws:sqs:us-east-1:147997141255:tech-challenge-fiap-upload-notifications"
+#   function_name    = aws_lambda_function.dotnet8_consumer.arn
+#   batch_size       = 10
+# }
